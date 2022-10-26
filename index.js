a4452789062c3ac2f4325655ce911bb94bfe929c
Ivan Bozveliev <ivanbozveliev@abv.bg>
2022-10-26 21:27:15 +0300

    fixed marker path logic

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 let markers = [];
-let data = [];
+let pathPositions = [];
 
 function initMap() {
   const uluru = { lat: -25.344, lng: 131.031 };
@@ -8,11 +8,10 @@ function initMap() {
     center: uluru,
     streetViewControl: false,
   });
-  const infowindow = new google.maps.InfoWindow();
 
   google.maps.event.addListener(map, "click", (event) => {
     addMarker(event.latLng, map);
-    // geocode({ location: event.latLng });
+    // geocode({ location: event.latLng }, map);
   });
 }
 
@@ -22,49 +21,58 @@ function addMarker(location, map) {
     map: map,
   });
   marker.setMap(map);
-  markers.push(marker.position);
+  markers.push(marker);
+  pathPositions.push(marker.position);
 
   if (markers.length === 2) {
-    setFlightPath(markers, map);
-
-    if (distance(markers) >= 3000) {
+    if (distance(markers) > 3000) {
+      setFlightPath(pathPositions, map);
       document.getElementById("miles").innerHTML =
         distance(markers) + "<p id='textMiles'> nautical miles</p>";
       hours(distance(markers), 1);
     } else {
+      markers.map((marker) => marker.setMap(null));
+      markers = [];
       document.getElementById("miles").innerHTML =
         "<p id='errorText'>You should select minimum 3000 miles distance</p>";
       document.getElementById("hours").innerHTML =
         "<p id='errorText'>-----</p>";
     }
 
-    data.push(markers);
     markers = [];
+    pathPositions = [];
   }
 }
 
-function geocode(location) {
+function geocode(location, map) {
   const geocoder = new google.maps.Geocoder();
   const infowindow = new google.maps.InfoWindow();
 
   geocoder.geocode(location).then((response) => {
     const { results } = response;
     console.log(results);
-    console.log(data);
     console.log(results[1].formatted_address);
-    console.log(distance(data));
-    // if (data.length % 2 == 0) {
-    //   distance(data);
-    // }
+
     infowindow.setContent(results[1].formatted_address);
     infowindow.open(map);
   });
 }
 
+function setFlightPath(positions, map) {
+  let flightPath = new google.maps.Polyline({
+    path: positions,
+    strokeColor: "#ff0208",
+    strokeOpacity: 1.0,
+    geodesic: true,
+    strokeWeight: 5,
+  });
+  flightPath.setMap(map);
+}
+
 function distance(data) {
   if (data.length !== 0) {
-    let mk1 = data[0];
-    let mk2 = data[1];
+    let mk1 = data[0].position;
+    let mk2 = data[1].position;
 
     var R = 3958.8; // Radius of the Earth in miles
     var rlat1 = mk1.lat() * (Math.PI / 180); // Convert degrees to radians
@@ -86,37 +94,6 @@ function distance(data) {
       );
     return d.toFixed(2);
   }
-}
-
-function setFlightPath(markers, map) {
-  // var lineSymbol = {
-  //   path: "M100,200 C100,100 400,100 400,200",
-  //   strokeOpacity: 1,
-  //   scale: 4,
-  // };
-
-  var lineSymbol = {
-    path: "M100 200C100 100 400 100 400 200",
-    strokeOpacity: 1,
-    scale: 4,
-  };
-
-  markers.map(() => {
-    let flightPath = new google.maps.Polyline({
-      path: markers,
-      strokeColor: "#ff0208",
-      strokeOpacity: 1.0,
-      geodesic: true,
-      strokeWeight: 5,
-      icons: [
-        {
-          icon: lineSymbol,
-        },
-      ],
-    });
-
-    flightPath.setMap(map);
-  });
 }
 
 function hours(distance, flights) {
